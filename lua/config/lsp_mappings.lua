d18c47141b573acc6413222fab8db36132391cf1
alunity <75143943+alunity@users.noreply.github.com>
2024-09-29 18:00:51 +0100

    Initial config

---
+++ lua/config/lsp_mappings.lua
@@ -0,0 +1,11 @@
+-- stuff i'm stealing from kickstart :)
+vim.keymap.set("n", 'gd', require('telescope.builtin').lsp_definitions)
+vim.keymap.set("n", 'gr', require('telescope.builtin').lsp_references)
+vim.keymap.set("n", 'gI', require('telescope.builtin').lsp_implementations)
+vim.keymap.set("n", '<leader>D', require('telescope.builtin').lsp_type_definitions)
+vim.keymap.set("n", '<leader>ds', require('telescope.builtin').lsp_document_symbols)
+vim.keymap.set("n", '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+vim.keymap.set("n", '<leader>rn', vim.lsp.buf.rename)
+vim.keymap.set("n", '<leader>ca', vim.lsp.buf.code_action)
+vim.keymap.set("n", 'gD', vim.lsp.buf.declaration)
+
---
+++ lua/config/plugins/lsp.lua
@@ -0,0 +1,146 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+    },
+
+    config = function()
+        local cmp = require('cmp')
+	local luasnip = require("luasnip")
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls",
+                "pyright",
+                "clangd",
+                "hls",
+                "harper_ls"
+            },
+            handlers = {
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
+                end,
+
+                zls = function()
+
+                    local lspconfig = require("lspconfig")
+                    lspconfig.zls.setup({
+                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                        settings = {
+                            zls = {
+                                enable_inlay_hints = true,
+                                enable_snippets = true,
+                                warn_style = true,
+                            },
+                        },
+                    })
+                    vim.g.zig_fmt_parse_errors = 0
+                    vim.g.zig_fmt_autosave = 0
+
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = { version = "Lua 5.1" },
+                                diagnostics = {
+                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                                }
+                            }
+                        }
+                    }
+                end,
+            }
+        })
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+		['<CR>'] = cmp.mapping(function(fallback)
+		    if cmp.visible() then
+			if luasnip.expandable() then
+			    luasnip.expand()
+			else
+			    cmp.confirm({
+				select = true,
+			    })
+			end
+		    else
+			fallback()
+		    end
+		end),
+
+		["<Tab>"] = cmp.mapping(function(fallback)
+		  if cmp.visible() then
+		    cmp.select_next_item()
+		  elseif luasnip.locally_jumpable(1) then
+		    luasnip.jump(1)
+		  else
+		    fallback()
+		  end
+		end, { "i", "s" }),
+
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+		  if cmp.visible() then
+		    cmp.select_prev_item()
+		  elseif luasnip.locally_jumpable(-1) then
+		    luasnip.jump(-1)
+		  else
+		    fallback()
+		  end
+		end, { "i", "s" }),
+		       }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+
+            }, {
+                { name = 'buffer' },
+            }),
+	    experimental = {
+		ghost_text = true,
+	    },
+        })
+
+
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+    end
+}
