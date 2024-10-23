ee5d9f8c95538c640a8ef73449200e65db9411ce
alunity <75143943+alunity@users.noreply.github.com>
2024-10-23 19:12:12 +0100

    Formatter :)

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,9 +1,10 @@
 vim.opt.nu = true
 -- vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.opt.expandtab = true
 
@@ -27,4 +28,3 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-
