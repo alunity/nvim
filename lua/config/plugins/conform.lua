43fc3eb73671462b048dce55a434e3a4765de0b0
alunity <75143943+alunity@users.noreply.github.com>
2026-04-08 00:52:24 +0100

    typst preview + nix formatter

--- lua/config/plugins/conform.lua
+++ lua/config/plugins/conform.lua
@@ -37,7 +37,8 @@ return { -- Autoformat
       -- Conform can also run multiple formatters sequentially
       python = { "black" },
       markdown = { "mdformat" },
-      json = {"prettier"}
+      json = {"prettier"},
+      nix = {"alejandra"}
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
